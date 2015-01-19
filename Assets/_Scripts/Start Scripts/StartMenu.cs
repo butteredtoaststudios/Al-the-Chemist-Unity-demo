9a4ec5ebce4a732a04269f4646385e98ac8a7a7f
sleepingsheep <sleepingsheepy@hotmail.com>
2015-01-19 11:56:39 -0500

    merge

--- Assets/_Scripts/Start Scripts/StartMenu.cs
+++ Assets/_Scripts/Start Scripts/StartMenu.cs
@@ -6,12 +6,17 @@ public class StartMenu : MonoBehaviour
 	public Sprite[] startButtons;
 
 	GameObject[] buttons = new GameObject[3];
-
+	
 	public bool selected = false;
 	int selectedOption = 0;
 
+	public Sprite logo;
+	private bool showLogo = true;
+
 	// Use this for initialization
 	void Start () {
+		Screen.SetResolution(720, 480, false); 
+
 		buttons[0] = GameObject.Find("Start");
 		buttons[1] = GameObject.Find("Options");
 		buttons[2] = GameObject.Find("Exit");
@@ -19,19 +24,36 @@ public class StartMenu : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		buttons[0].GetComponent<SpriteRenderer>().sprite = startButtons[0];
-		buttons[1].GetComponent<SpriteRenderer>().sprite = startButtons[2];
-		buttons[2].GetComponent<SpriteRenderer>().sprite = startButtons[4];
+		if(!showLogo)
+		{
+			buttons[0].GetComponent<SpriteRenderer>().sprite = startButtons[0];
+			buttons[1].GetComponent<SpriteRenderer>().sprite = startButtons[2];
+			buttons[2].GetComponent<SpriteRenderer>().sprite = startButtons[4];
 
-		if(selectedOption == 0)
-			buttons[0].GetComponent<SpriteRenderer>().sprite = startButtons[1];
-		else if(selectedOption == 1)
-			buttons[1].GetComponent<SpriteRenderer>().sprite = startButtons[3];
-		else if(selectedOption == 2)
-			buttons[2].GetComponent<SpriteRenderer>().sprite = startButtons[5];
+			if(selectedOption == 0)
+				buttons[0].GetComponent<SpriteRenderer>().sprite = startButtons[1];
+			else if(selectedOption == 1)
+				buttons[1].GetComponent<SpriteRenderer>().sprite = startButtons[3];
+			else if(selectedOption == 2)
+				buttons[2].GetComponent<SpriteRenderer>().sprite = startButtons[5];
 
-		if(!selected)
-			userControl();
+			if(!selected)
+				userControl();
+		}
+		else
+		{
+			StartCoroutine("logoTime");
+		}
+	}
+
+	public IEnumerator logoTime()
+	{
+		yield return new WaitForSeconds(2);
+
+		_alpha -= 0.05f;
+
+		if(_alpha <= 0.01)
+			showLogo = false;
 	}
 
 	void userControl()
@@ -46,7 +68,7 @@ public class StartMenu : MonoBehaviour
 			switch(selectedOption)
 			{
 			case 0:
-				Application.LoadLevel("01_Lab");
+				Application.LoadLevel("00_Tutorial");
 				break;
 			case 1:
 				selected = true;
@@ -57,5 +79,15 @@ public class StartMenu : MonoBehaviour
 				break;
 			}
 		}
+	}
+
+	private float _alpha = 1.0f;
+
+	void OnGUI()
+	{
+		GUI.color = new Color(1, 1, 1, _alpha);
+
+		if(showLogo)
+			GUI.Box(new Rect(0, 0, 720, 480), logo.texture, "");
 	}
 }
